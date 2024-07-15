2f023e5334eda06b32091701b0b69eb9175ed8a3
Pandi Sundaram <pandi.sundaram@anywhere.co>
2024-07-15 21:57:34 +0530

    base setup added

---
+++ src/main/java/com/hackathon/team52/talent_tap/TalentTapApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.team52.talent_tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentTapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentTapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/team52/talent_tap/TalentTapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.team52.talent_tap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentTapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
