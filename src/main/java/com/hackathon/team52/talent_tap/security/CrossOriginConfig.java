1ac8650b54e225ac78a381ff80ad6b0d62fc936d
Pandi Sundaram <pandi.sundaram@anywhere.co>
2024-07-16 15:05:09 +0530

    config updated

---
+++ src/main/java/com/hackathon/team52/talent_tap/security/CrossOriginConfig.java
@@ -0,0 +1,17 @@
+package com.hackathon.team52.talent_tap.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow specific HTTP methods
+                .allowedHeaders("*");  // Allow all headers
+    }
+
+}
